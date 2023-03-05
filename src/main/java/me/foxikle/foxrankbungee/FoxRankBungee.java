3da3f3ed3808ce2431f46312ff1d956204730dea
Foxikle <tristantroop260@gmail.com>
2023-03-05 07:23:59 -0500

    Initial commit

---
+++ src/main/java/me/foxikle/foxrankbungee/FoxRankBungee.java
@@ -0,0 +1,11 @@
+package me.foxikle.foxrankbungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class FoxRankBungee extends Plugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+}
